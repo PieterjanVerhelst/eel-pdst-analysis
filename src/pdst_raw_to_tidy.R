58586588345a2a3f919d06b10741230b66b563a6
stijnvanhoey <stijnvanhoey@gmail.com>
2019-01-24 22:22:19 +0100

    Add read processing file

---
+++ src/pdst_raw_to_tidy.R
@@ -0,0 +1,42 @@
+#
+# pdst sensor read functionalities
+# 
+# Van Hoey S. 2019
+# Lifewatch INBO
+# 
+
+library(tidyverse)
+
+# load the reading functions
+source("./src/pdst_functions.R")
+
+# get an overview of the files
+main_data_dir <- "./data/raw"
+data_files <- list.files(main_data_dir, full.names = TRUE)
+
+# create interim dictionary if not already there
+interim_data_dir <- file.path("data", "interim")
+if (!dir.exists(interim_data_dir)) {
+  dir.create(interim_data_dir)
+}
+
+# read data from the raw data files and convert to tidy -----
+
+data_files <- data_files[1:2] # TESTPHASE
+
+for (filename in data_files) {
+  
+  # use custom read functionality
+  file_data <- pdst_read_file(filename)
+  
+  # write the daylog and sensor as tidy data files
+  write_csv(file_data$daylog, 
+            file.path(interim_data_dir, 
+                      paste0("daylog_", basename(filename))))
+  write_csv(file_data$sensor, 
+            file.path(interim_data_dir, 
+                      paste0("sensor_", basename(filename))))
+}
+
+
+
