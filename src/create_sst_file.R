8975c36c1142ed36da2dbdada0f22ac9f113348d
Pieterjan Verhelst <Pieterjan.Verhelst@UGent.be>
2020-02-25 16:40:17 +0000

    Update create_sst_file.R

--- src/create_sst_file.R
+++ src/create_sst_file.R
@@ -8,67 +8,59 @@ library(tidyverse)
 library(lubridate)
 
 
-# 1. Read in sensor data ####
-sensordata <- read_csv("./data/interim/sensor_A17443_27-01-2020.csv")
+# 1. Read in temperature and corrected pressure data ####
+temp_data <- read_csv("./data/interim/input_A17443/EELA17443TEMP.csv")
+press_data <- read_csv("./data/interim/input_A17443/EELA17443PRES.csv")
 
-
-# 2. Aggregate data ####
-# Hence, make sure temperature and depth data are of equal length
-sensordata$datetime <- dmy_hms(sensordata$datetime)
-sensordata$datetime2 <- droplevels(cut(sensordata$datetime, breaks="5 min"))   # 5 min cut
-
-aggdata <- aggregate(cbind(pressure, temperature) ~ datetime2, data=sensordata, FUN=mean, na.rm=TRUE) 
-aggdata$datetime2 <- ymd_hms(aggdata$datetime2)
-
-# Reverse depth
-#aggdata$pressure <- aggdata$pressure * -1   # Not needed
+# Merge them together
+temp_press <- merge(temp_data, press_data, by="Date")
 
 
 # Set release and retrieval to create plots
-release <- "2019-11-02 18:15:00"
+release <- "2019-11-02 18:00:00"
 retrieval <- "2019-12-06 12:00:00"
 
 
-# 3. Subset from release to retrieval date ####
+# 2. Subset from release to retrieval date ####
 # Note for first day, take only the values since release
-subset <- filter(aggdata, datetime2 >= release, datetime2 <= "2019-11-02 23:55:00")
+subset <- filter(temp_press , Date >= release, Date <= "2019-11-02 23:55:00")
 
-subset <- filter(aggdata, datetime2 >= "2019-12-06 00:00:00", datetime2 <= "2019-12-06 23:55:00")
+subset <- filter(temp_press , Date >= "2019-12-06 00:00:00", Date <= "2019-12-06 23:55:00")
 
 # summary to check max depth
 summary(subset)
 
 # max depth
-max(subset$pressure)
+max(subset$Depth)
 
 # average temp
-mean(subset$temperature)
+mean(subset$Temp)
 
 # In case depth > 20 m, create subset to calculate sea surface temperature
-subset_sst <- filter(subset, pressure <= 20)
-mean(subset_sst$temperature)
+subset_sst <- filter(subset, Depth <= 20)
+mean(subset_sst$Temp)
 
 
 
-# 4. Create file and fill in values manually ####
-input_sst <- data.frame(seq(as.Date("2019/11/02"), as.Date("2019/12/06"), "days"))
-colnames(input_sst)[1] <- "Date/Time Stamp"
+# 3. Create file and fill in values manually ####
+#input_sst <- data.frame(seq(as.Date("2019/11/02"), as.Date("2019/12/06"), "days"))
+#colnames(input_sst)[1] <- "Date/Time Stamp"
 
-input_sst$Temp <- NA
-input_sst$SST_depth <- NA
-input_sst$Max_Pressure <- NA
+#input_sst$Temp <- NA
+#input_sst$SST_depth <- NA
+#input_sst$Max_Pressure <- NA
 
 
 # Run in values manually
 i = 35
 
-input_sst$Temp[i] <-   15.36719
+input_sst$Temp[i] <-   mean(subset_sst$Temp)
 input_sst$SST_depth[i] <- 0
-input_sst$Max_Pressure[i] <- 8.236667
+input_sst$Max_Pressure[i] <- max(subset$Depth)
 
 
 
-# 5. Write csv files ####
+# 4. Write csv files ####
 write.csv(input_sst, "./data/interim/input_A17443/EELA17443TEMP_F.csv", row.names = FALSE)
 
 
