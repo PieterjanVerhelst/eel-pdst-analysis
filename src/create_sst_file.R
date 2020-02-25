596530dff2b12317204b3aaa6bee0613d044faf6
Pieterjan Verhelst <Pieterjan.Verhelst@UGent.be>
2020-02-25 19:31:00 +0000

    rearange code

--- src/create_sst_file.R
+++ src/create_sst_file.R
@@ -9,21 +9,30 @@ library(lubridate)
 
 
 # 1. Read in temperature and corrected pressure data ####
-temp_data <- read_csv("./data/interim/input_A16031/EELA16031TEMP.csv")
-press_data <- read_csv("./data/interim/input_A16031/EELA16031PRES.csv")
+temp_data <- read_csv("./data/interim/input_A15714/EELA15714TEMP.csv")
+press_data <- read_csv("./data/interim/input_A15714/EELA15714PRES.csv")
 
 # Merge them together
 temp_press <- merge(temp_data, press_data, by="Date")
 
 
 # Set release and retrieval to create plots
-release <- "2018-12-09 19:15:00"
-retrieval <- "2019-03-24 12:00:00"
+release <- "2018-12-04 14:00:00"
+retrieval <- "2019-01-22 12:00:00"
 
 
-# 2. Subset from release to retrieval date ####
+# 2. Create file and fill in values manually ####
+input_sst <- data.frame(seq(as.Date("2018/12/04"), as.Date("2019/01/22"), "days"))
+colnames(input_sst)[1] <- "Date/Time Stamp"
+
+input_sst$Temp <- NA
+input_sst$SST_depth <- NA
+input_sst$Max_Pressure <- NA
+
+
+# 3. Subset from release to retrieval date ####
 # Note for first day, take only the values since release
-subset <- filter(temp_press , Date >= release, Date <= "2018-12-09 23:55:00")
+subset <- filter(temp_press , Date >= release, Date <= "2018-12-04 23:55:00")
 
 subset <- filter(temp_press , Date >= "2019-03-24 00:00:00", Date <= "2019-03-24 23:55:00")
 
@@ -42,15 +51,6 @@ mean(subset_sst$Temp)
 
 
 
-# 3. Create file and fill in values manually ####
-input_sst <- data.frame(seq(as.Date("2018/12/09"), as.Date("2019/03/24"), "days"))
-colnames(input_sst)[1] <- "Date/Time Stamp"
-
-input_sst$Temp <- NA
-input_sst$SST_depth <- NA
-input_sst$Max_Pressure <- NA
-
-
 # Run in values manually
 i = 106
 
@@ -61,7 +61,7 @@ input_sst$Max_Pressure[i] <- max(subset$Depth)
 
 
 # 4. Write csv files ####
-write.csv(input_sst, "./data/interim/input_A16031/EELA16031TEMP_F.csv", row.names = FALSE)
+write.csv(input_sst, "./data/interim/input_A15714/EELA15714TEMP_F.csv", row.names = FALSE)
 
 
 
