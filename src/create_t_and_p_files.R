27c286b27957467d4f0af7c8878b09121242d070
Pieterjan Verhelst <Pieterjan.Verhelst@UGent.be>
2020-02-28 16:31:37 +0000

    include both date-time stamp columns

--- src/create_t_and_p_files.R
+++ src/create_t_and_p_files.R
@@ -33,27 +33,27 @@ subset <- filter(aggdata, datetime2 >= release, datetime2 <= retrieval)
 
 # 4. Select temperature data only ####
 temp <- select(subset, datetime2, temperature)
-colnames(temp)[1] <- "Date"
+colnames(temp)[1] <- "Date2"
 colnames(temp)[2] <- "Temp"
 
 # Set in correct date-time format dd/mm/yy HH:MM
 #temp$Date <- format(as.POSIXct(temp$Date, format = "%y%m%d %H:%M:%S"), "%d/%m/%Y %H:%M:%S")  # with seconds
-temp$Date <- format(as.POSIXct(temp$Date, format = "%y%m%d %H:%M:%S"), "%d/%m/%Y %H:%M")  # without seconds
+temp$Date <- format(as.POSIXct(temp$Date2, format = "%y%m%d %H:%M:%S"), "%d/%m/%Y %H:%M")  # without seconds
 
 
 # 5. Select pressure data only ####
 press <- select(subset, datetime2, pressure)
-colnames(press)[1] <- "Date"
+colnames(press)[1] <- "Date2"
 colnames(press)[2] <- "Depth"
 
 # Set in correct date-time format dd/mm/yy HH:MM
 # Note that the correct date time format is stored in a different column. The original column (yyyy-mm-dd hh:mm:ss) is needed for drift correctino (see further)
 #temp$Date2 <- format(as.POSIXct(temp$Date, format = "%y%m%d %H:%M:%S"), "%d/%m/%Y %H:%M:%S")  # with seconds
-press$Date2 <- format(as.POSIXct(press$Date, format = "%y%m%d %H:%M:%S"), "%d/%m/%Y %H:%M")  # without seconds
+press$Date <- format(as.POSIXct(press$Date2, format = "%y%m%d %H:%M:%S"), "%d/%m/%Y %H:%M")  # without seconds
 
 
 # 6. Correct for pressure sensor drift ####
-plot(press$Date, press$Depth)
+plot(press$Date2, press$Depth)
 # Select date: moment of release - 15 min and pop-off moment (moment it was certainly at the surface)
 subset2 <- filter(aggdata, datetime2 == "2018-12-09 19:00:00" | datetime2 == "2019-02-17 00:00:00")
 plot(subset2$datetime2, subset2$pressure)
@@ -61,28 +61,26 @@ abline(lm(subset2$pressure ~ subset2$datetime2))
 lm(subset2$pressure ~ subset2$datetime2)  # To get coefficient and estimates
 # depth = (2.322e-05 * date)  -3.587e+04
 
-press$numericdate <- as.numeric(press$Date)
+press$numericdate <- as.numeric(press$Date2)
 press$regression <- (2.787e-07 *press$numericdate)   -4.298e+02
 press$corrected_depth <- press$Depth-press$regression
 
 
 # Some check diagnostics
 press$diff <- press$Depth-press$corrected_depth
-plot(press$Date, press$diff)
-plot(press$Date, press$Depth)
-plot(press$Date, press$corrected_depth)
-check <- filter(press, Date >= "2019-11-02 18:15:00", Date <= "2019-11-28 10:00:00")
+plot(press$Date2, press$diff)
+plot(press$Date2, press$Depth)
+plot(press$Date2, press$corrected_depth)
+check <- filter(press, Date2 >= "2019-11-02 18:15:00", Date2 <= "2019-11-28 10:00:00")
 summary(check)
 
 
 # rearrange pressure file
-press$Date <- NULL
 press$Depth <- NULL
 press$numericdate <- NULL
 press$regression <- NULL
 press$diff <- NULL
 press <- rename(press, Depth = corrected_depth)
-press <- rename(press, Date = Date2)
 
 
 # 7. Write csv files ####
