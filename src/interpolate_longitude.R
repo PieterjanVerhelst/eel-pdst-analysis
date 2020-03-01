ac78be394090ae2b6a43f0575487f2dede2518a4
Pieterjan Verhelst <Pieterjan.Verhelst@UGent.be>
2020-03-01 21:47:14 +0000

    Update interpolate_longitude.R

--- src/interpolate_longitude.R
+++ src/interpolate_longitude.R
@@ -4,18 +4,18 @@
 
 # Create dataset with the following columns: Date/Time Stamp,	Long,	Quality
 # Date from eel release until tag retrieval
-input1 <- data.frame(seq(as.Date("2018/11/11"), as.Date("2018/12/01"), "days")) # take day before retrieval, since exact moment of retrieval is unknown
+input1 <- data.frame(seq(as.Date("2018/11/11"), as.Date("2018/11/30"), "days")) # take day before retrieval, since exact moment of retrieval is unknown
 colnames(input1)[1] <- "Date/Time Stamp"
 
 
 # Interpolate between longitude of release and retrieval location
-input1$Long <- seq(2.76, -0.64, length.out = 21)
+input1$Long <- seq(2.76, -0.64, length.out = 20)
 
 # Add quality flag to longitude values
 # First and last line have highest certainty and therefore quality; these are flagged with '0'. Other get '5' (= worse quality)
 input1$Quality <- 5
 input1$Quality[1] <- 0
-input1$Quality[21] <- 0
+input1$Quality[20] <- 0
 
 
 # Write csv file
