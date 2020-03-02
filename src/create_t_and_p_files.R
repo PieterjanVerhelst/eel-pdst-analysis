2b8614b3b311366e8fd0ea4ee4655a2b6bc31ad4
Pieterjan Verhelst <Pieterjan.Verhelst@UGent.be>
2020-03-02 12:14:08 +0000

    create input files for eel 17528

--- src/create_t_and_p_files.R
+++ src/create_t_and_p_files.R
@@ -10,7 +10,7 @@ library(lubridate)
 
 
 # 1. Read in sensor data ####
-sensordata <- read_csv("./data/interim/sensor_A17513_20-02-2020.csv")
+sensordata <- read_csv("./data/interim/sensor_A17528_20-02-2020.csv")
 
 
 # 2. Aggregate data ####
@@ -25,8 +25,8 @@ aggdata$datetime2 <- ymd_hms(aggdata$datetime2)
 #aggdata$pressure <- aggdata$pressure * -1   # Not needed
 
 # Set release and retrieval or pop off time (midday following popping event)
-release <- "2019-12-14 14:00:00"
-retrieval <- "2020-01-24 23:55:00"  # Take day before retrieval, since exact moment of retrieval is unknown
+release <- "2019-12-10 13:35:00"
+retrieval <- "2020-01-19 23:55:00"  # Take day before retrieval, since exact moment of retrieval is unknown
 
 # 3. Subset from release to retrieval date ####
 subset <- filter(aggdata, datetime2 >= release, datetime2 <= retrieval)
@@ -59,14 +59,14 @@ press$Date <- format(as.POSIXct(press$Date2, format = "%y%m%d %H:%M:%S"), "%d/%m
 # 6. Correct for pressure sensor drift ####
 plot(press$Date2, press$Depth)
 # Select date: moment of release - 15 min and pop-off moment (moment it was certainly at the surface)
-subset2 <- filter(aggdata, datetime2 == "2019-12-14 13:45:00" | datetime2 == "2020-01-24 23:55:00")
+subset2 <- filter(aggdata, datetime2 == "2019-12-10 13:20:00" | datetime2 == "2020-01-17 23:55:00")
 plot(subset2$datetime2, subset2$pressure)
 abline(lm(subset2$pressure ~ subset2$datetime2))
 lm(subset2$pressure ~ subset2$datetime2)  # To get coefficient and estimates
 # depth = (2.322e-05 * date)  -3.587e+04
 
 press$numericdate <- as.numeric(press$Date2)
-press$regression <- ( 2.016e-06  *press$numericdate)     -3.176e+03
+press$regression <- (3.357e-06   *press$numericdate)     -5.290e+03
 press$corrected_depth <- press$Depth-press$regression
 
 
@@ -89,8 +89,8 @@ press <- rename(press, Depth = corrected_depth)
 
 
 # 7. Write csv files ####
-write.csv(temp, "./data/interim/input_A17513/EELA17513TEMP.csv", row.names = FALSE)
-write.csv(press, "./data/interim/input_A17513/EELA17513PRES.csv", row.names = FALSE)
+write.csv(temp, "./data/interim/input_A17528/EELA17528TEMP.csv", row.names = FALSE)
+write.csv(press, "./data/interim/input_A17528/EELA17528PRES.csv", row.names = FALSE)
 
 
 
--- src/interpolate_longitude.R
+++ src/interpolate_longitude.R
@@ -4,19 +4,19 @@
 
 # Create dataset with the following columns: Date/Time Stamp,	Long,	Quality
 # Date from eel release until tag retrieval
-input1 <- data.frame(seq(as.Date("2019/12/14"), as.Date("2020/01/24"), "days")) # take day before retrieval, since exact moment of retrieval is unknown
+input1 <- data.frame(seq(as.Date("2019/12/10"), as.Date("2020/01/19"), "days")) # take day before retrieval, since exact moment of retrieval is unknown
 colnames(input1)[1] <- "Date/Time Stamp"
 
 
 # Interpolate between longitude of release and retrieval location
-input1$Long <- seq(2.76, -0.86, length.out = 42)
+input1$Long <- seq(2.76, 5.22, length.out = 41)
 
 # Add quality flag to longitude values
 # First and last line have highest certainty and therefore quality; these are flagged with '0'. Other get '5' (= worse quality)
 input1$Quality <- 5
 input1$Quality[1] <- 0
-input1$Quality[42] <- 0
+input1$Quality[41] <- 0
 
 
 # Write csv file
-write.csv(input1, "./data/interim/input_A17513/EELA17513LONG.csv", row.names = FALSE)
+write.csv(input1, "./data/interim/input_A17528/EELA17528LONG.csv", row.names = FALSE)
