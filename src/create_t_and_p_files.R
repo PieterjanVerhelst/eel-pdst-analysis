64a1d4090a9ee131ebc9307431a6a203775c7f77
Pieterjan Verhelst <Pieterjan.Verhelst@UGent.be>
2020-03-02 09:37:12 +0000

    create input files for eel 15730

--- src/create_t_and_p_files.R
+++ src/create_t_and_p_files.R
@@ -10,7 +10,7 @@ library(lubridate)
 
 
 # 1. Read in sensor data ####
-sensordata <- read_csv("./data/interim/sensor_A17443_27-01-2020.csv")
+sensordata <- read_csv("./data/interim/sensor_A15730_15-01-2019.csv")
 
 
 # 2. Aggregate data ####
@@ -25,8 +25,8 @@ aggdata$datetime2 <- ymd_hms(aggdata$datetime2)
 #aggdata$pressure <- aggdata$pressure * -1   # Not needed
 
 # Set release and retrieval or pop off time (midday following popping event)
-release <- "2019-11-02 18:15:00"
-retrieval <- "2019-12-05 23:55:00"  # Take day before retrieval, since exact moment of retrieval is unknown
+release <- "2018-10-31 20:00:00"
+retrieval <- "2018-11-30 23:55:00"  # Take day before retrieval, since exact moment of retrieval is unknown
 
 # 3. Subset from release to retrieval date ####
 subset <- filter(aggdata, datetime2 >= release, datetime2 <= retrieval)
@@ -59,14 +59,14 @@ press$Date <- format(as.POSIXct(press$Date2, format = "%y%m%d %H:%M:%S"), "%d/%m
 # 6. Correct for pressure sensor drift ####
 plot(press$Date2, press$Depth)
 # Select date: moment of release - 15 min and pop-off moment (moment it was certainly at the surface)
-subset2 <- filter(aggdata, datetime2 == "2019-11-02 18:00:00" | datetime2 == "2019-12-04 23:55:00")
+subset2 <- filter(aggdata, datetime2 == "2018-11-11 00:00:00" | datetime2 == "2018-11-30 23:55:00")
 plot(subset2$datetime2, subset2$pressure)
 abline(lm(subset2$pressure ~ subset2$datetime2))
 lm(subset2$pressure ~ subset2$datetime2)  # To get coefficient and estimates
 # depth = (2.322e-05 * date)  -3.587e+04
 
 press$numericdate <- as.numeric(press$Date2)
-press$regression <- ( 2.145e-06 *press$numericdate)   -3.372e+03
+press$regression <- ( 2.633e-06 *press$numericdate)    -4.059e+03
 press$corrected_depth <- press$Depth-press$regression
 
 
@@ -89,8 +89,8 @@ press <- rename(press, Depth = corrected_depth)
 
 
 # 7. Write csv files ####
-write.csv(temp, "./data/interim/input_A17443/EELA17443TEMP.csv", row.names = FALSE)
-write.csv(press, "./data/interim/input_A17443/EELA17443PRES.csv", row.names = FALSE)
+write.csv(temp, "./data/interim/input_A15730/EELA15730TEMP.csv", row.names = FALSE)
+write.csv(press, "./data/interim/input_A15730/EELA15730PRES.csv", row.names = FALSE)
 
 
 
