d83bd1d85693fba3e1aef1f1bb455748638916e6
Pieterjan Verhelst <Pieterjan.Verhelst@UGent.be>
2020-03-02 18:47:32 +0000

    create input file for 15799

--- src/create_sst_file.R
+++ src/create_sst_file.R
@@ -9,8 +9,8 @@ library(lubridate)
 
 
 # 1. Read in temperature and corrected pressure data ####
-temp_data <- read_csv("./data/interim/input_A15777/EELA15777TEMP.csv")
-press_data <- read_csv("./data/interim/input_A15777/EELA15777PRES.csv")
+temp_data <- read_csv("./data/interim/input_A15799/EELA15799TEMP.csv")
+press_data <- read_csv("./data/interim/input_A15799/EELA15799PRES.csv")
 
 
 # Merge them together
@@ -54,7 +54,7 @@ input_sst <- input_sst[,c(1,3,4,2)]
 
 
 # 4. Write csv files ####
-write.csv(input_sst, "./data/interim/input_A15777/EELA15777TEMP_F.csv", na = "NaN", row.names = FALSE)
+write.csv(input_sst, "./data/interim/input_A15799/EELA15799TEMP_F.csv", na = "NaN", row.names = FALSE)
 
 
 
