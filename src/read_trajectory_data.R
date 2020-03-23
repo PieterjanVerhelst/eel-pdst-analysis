0b73daeaa2b79dd37a9acfff13f9b141eb66d65d
Pieterjan Verhelst <Pieterjan.Verhelst@UGent.be>
2020-03-23 17:55:12 +0100

    batch read trajectory data and process concatenated file

--- src/read_trajectory_data.R
+++ src/read_trajectory_data.R
@@ -5,5 +5,19 @@
 library(tidyverse)
 
 
-# 1. Read in trajectory data ####
-tr_data <- read_csv("./data/external/trajectory_data/EEL16031_Summary.csv")
+# Read in trajectory data ####
+tr_data <-  list.files(path = "./data/external/trajectory_data/",
+             pattern = "*.csv", 
+             full.names = T) %>% 
+  map_df(~read_csv(., col_types = cols(.default = "c"))) 
+
+
+
+# Select columns
+tr_data <- select(tr_data, ID, Date, MPL.Hori_Dist_km)
+tr_data <- rename(tr_data, Distance = MPL.Hori_Dist_km)
+
+
+# Remove NA
+sum(is.na(tr_data$Distance))
+tr_data <- na.omit(tr_data) 
