089d8931d562dd032164279db1a5d6e564d31cb1
Pieterjan Verhelst <Pieterjan.Verhelst@UGent.be>
2020-03-24 15:39:05 +0100

    Update analyse_migration_speed.R

--- src/analyse_migration_speed.R
+++ src/analyse_migration_speed.R
@@ -34,29 +34,101 @@ tr_summary <- merge(tr_summary, direction, by = "ID")
 
 
 # Data exploration
+
+# 1. All eels
 boxplot(speed ~ Direction, data = tr_summary)
 tr_summary %>%
   group_by(Direction) %>%
   summarize(mean = mean(speed))
+min(tr_summary$total_dist)
+max(tr_summary$total_dist)
+min(tr_summary$days)
+max(tr_summary$days)
 
-tr_summary2 <- filter(tr_summary, ID == '9358'|
-                      ID == '9374' |
+# 2. Eels with nice tracks
+tr_summary2 <- filter(tr_summary, ID == '9349' |
+                        ID == '9355' |
+                        ID == '9358' |
+                        ID == '9359' |
+                        ID == '9374' |
                         ID == '9377' |
                         ID == '9393' |
                         ID == '9423' |
                         ID == '9424' |
                         ID == '15706' |
                         ID == '15714' |
+                        ID == '15730' |
                         ID == '15777' |
                         ID == '15805' |
                         ID == '15981' |
                         ID == '16031' |
-                        ID == '112061')
+                        ID == '112061' |
+                        ID == '112064')
 
 boxplot(speed ~ Direction, data = tr_summary2)
 tr_summary2 %>%
   group_by(Direction) %>%
   summarize(mean = mean(speed))
+min(tr_summary2$total_dist)
+max(tr_summary2$total_dist)
+min(tr_summary2$days)
+max(tr_summary2$days)
+
+
+# 3. Eels with crappy tracks
+tr_summary3 <- filter(tr_summary, ID != '9349' ,
+                        ID != '9355' ,
+                        ID != '9358' ,
+                        ID != '9359' ,
+                        ID != '9374' ,
+                        ID != '9377' ,
+                        ID != '9393' ,
+                        ID != '9423' ,
+                        ID != '9424' ,
+                        ID != '15706' ,
+                        ID != '15714' ,
+                        ID != '15730' ,
+                        ID != '15777' ,
+                        ID != '15805' ,
+                        ID != '15981' ,
+                        ID != '16031' ,
+                        ID != '112061' ,
+                        ID != '112064')
+
+boxplot(speed ~ Direction, data = tr_summary3)
+tr_summary3 %>%
+  group_by(Direction) %>%
+  summarize(mean = mean(speed))
+min(tr_summary3$total_dist)
+max(tr_summary3$total_dist)
+min(tr_summary3$days)
+max(tr_summary3$days)
+
+
+
+# Data analysis
+
+# independent 2-group t-test
+# t.test(y~x) # where y is numeric and x is a binary factor 
+t.test(tr_summary2$speed~tr_summary2$Direction)
+
+# Assumptions:
+# 1. 2 independent groups
+# N and SW are independent (each direction is attributed to a different animal)
+
+# 2. Normality
+qqnorm(tr_summary2$speed, pch = 1, frame = TRUE)
+qqline(tr_summary2$speed, col = "steelblue", lwd = 2)
+
+shapiro.test(tr_summary2$speed)
+# The p-value > 0.05 implying that the distribution of the data are not significantly different from normal distribution. In other words, we can assume the normality.
+
+# 3. Homogeneity of variances
+# F-test 
+# The F-test is used to assess whether the variances of two populations (A and B) are equal. The test requires normality (in case no normality, apply Fligner-Killeen’s test)
+# https://www.datanovia.com/en/lessons/homogeneity-of-variance-test-in-r/
+var.test(speed ~ Direction, data = tr_summary2)
+# When p > 0.05, there is no significant difference between the two variances.
 
 
 
