39ec50674c41d0e80178acd5c2fe8d629a66f546
Pieterjan Verhelst <Pieterjan.Verhelst@UGent.be>
2020-05-26 17:12:48 +0200

    Update create_sensor_file_all_eels.R

--- src/create_sensor_file_all_eels.R
+++ src/create_sensor_file_all_eels.R
@@ -186,7 +186,7 @@ all_sensor_eels_processed <- do.call("rbind", list(eel_A09359,
                                             eel_A15777,
                                             eel_A16031))
 
-#write.csv(all_sensor_eels_processed, "./interim/all_sensor_eels_processed.csv")
+write.csv(all_sensor_eels_processed, "./interim/all_sensor_eels_processed.csv")
 
 
 
