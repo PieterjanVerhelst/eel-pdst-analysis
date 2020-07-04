7d8d8cf6c146cd4ca0275d7b6f9974a747405fb6
Pieterjan Verhelst <Pieterjan.Verhelst@UGent.be>
2020-07-04 17:43:30 +0200

    create latitude for two new pdst datasets

--- src/interpolate_longitude.R
+++ src/interpolate_longitude.R
@@ -4,19 +4,19 @@
 
 # Create dataset with the following columns: Date/Time Stamp,	Long,	Quality
 # Date from eel release until tag retrieval
-input1 <- data.frame(seq(as.Date("2019/12/17"), as.Date("2020/03/10"), "days")) # take day before retrieval, since exact moment of retrieval is unknown
+input1 <- data.frame(seq(as.Date("2019/12/17"), as.Date("2020/02/15"), "days")) # take day before retrieval, since exact moment of retrieval is unknown
 colnames(input1)[1] <- "Date/Time Stamp"
 
 
 # Interpolate between longitude of release and retrieval location
-input1$Long <- seq(2.757437, 1.570457, length.out = 85)
+input1$Long <- seq(2.757437, -12.7, length.out = 61)
 
 # Add quality flag to longitude values
 # First and last line have highest certainty and therefore quality; these are flagged with '0'. Other get '5' (= worse quality)
 input1$Quality <- 5
 input1$Quality[1] <- 0
-input1$Quality[85] <- 0
+input1$Quality[61] <- 0
 
 
 # Write csv file
-write.csv(input1, "./data/interim/geolocation_input_files/input_A17537/EELA17537LONG.csv", row.names = FALSE)
+write.csv(input1, "./data/interim/geolocation_input_files/input_A15789/EELA15789LONG.csv", row.names = FALSE)
