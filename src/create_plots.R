5260a39e6c8d76b1d5ff86599969cd6ae2b325f2
Pieterjan Verhelst <Pieterjan.Verhelst@UGent.be>
2020-08-03 18:56:06 +0200

    create plots with depth difference

--- src/create_plots.R
+++ src/create_plots.R
@@ -9,6 +9,9 @@ library(tidylog)  # To get infos about dplyr functions
 library(lubridate)
 
 
+
+# 1. Plot raw data ####
+
 # Import data
 data <- read_csv("./data/interim/data_circadian_tidal.csv",
                  na = "", 
@@ -28,7 +31,7 @@ subset <- filter(data,
 # Create subset of several days for plot
 subset <- filter(data,
                  ID == "16031",
-                 datetime >= "2019-02-08 00:00:00", datetime <= "2019-02-13 00:00:00")
+                 datetime >= "2018-12-15 00:00:00", datetime <= "2018-12-20 00:00:00")
 
 # Create line every 24 hours
 gnu <-  seq.POSIXt(from = lubridate::floor_date(subset$datetime[1], "day"), to= subset$datetime[nrow(subset)], by = 86400)
@@ -68,8 +71,69 @@ fig_circadian_tidal
 
 
 
+# 2. Plot difference in depth between minima and maxima ####
+source("./src/calc_depth_diff.R")
+
+# Create plot with day night #
+# Create subsets of several days
+subset <- filter(data_min_max,
+                 ID == "16031",
+                 datetime >= "2018-12-15 00:00:00", datetime <= "2018-12-20 00:00:00")
+
+# Create line every 24 hours
+gnu <-  seq.POSIXt(from = lubridate::floor_date(subset$datetime[1], "day"), to= subset$datetime[nrow(subset)], by = 86400)
+class(lubridate::floor_date(subset$datetime[1], "day"))
+
+# Create plot
+fig_depth_diff_circadian_tidal <- ggplot(data = subset, aes(x = datetime, y = depth_change), size = 1.0, alpha = 0.5, colour = "black") +
+  geom_rect(data = subset %>% 
+              filter(night_day == "night") %>%
+              distinct(sunset, sunrise, night_day),
+            inherit.aes = FALSE,
+            mapping = aes(xmin = sunset,
+                          xmax = sunrise,
+                          ymin=-Inf,
+                          ymax=+Inf), fill = "grey", alpha=0.5) +
+  geom_line(binaxis='x', size=1.0, binwidth = 1) +
+  geom_line(data = subset, aes(x = datetime, y = direction/4), size = 2.0, alpha = 0.5, colour = "purple") +
+  #scale_y_continuous(breaks = seq(8.000, 12.000, by = 500)) +
+  #scale_y_continuous(sec.axis = sec_axis(~.*2, name = "Pressure (m)")) +
+  scale_y_continuous(sec.axis = sec_axis(~.*4, name = "Tidal direction (°)")) +
+  theme_minimal() +
+  ylab("Depth difference (m)") +
+  xlab("Date") +
+  theme(axis.title.y = element_text(margin = margin(r = 10))) +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
+  theme(axis.text = element_text(size = 12),
+        axis.title = element_text(size = 14)) +
+  scale_x_datetime(date_breaks  ="1 hour") +
+  #geom_vline(xintercept=ymd_hms(release), colour="blue") + # Release date and time
+  geom_vline(xintercept=gnu, color = "red", size = 1) +
+  geom_hline(yintercept=0, linetype="dashed", color = "blue", size = 1) +
+  geom_hline(yintercept=45, linetype="dashed", color = "green", size = 1) +
+  geom_hline(yintercept=-45, linetype="dashed", color = "green", size = 1)
+fig_depth_diff_circadian_tidal
 
 
+# Create plot with depth change per binned current direction
+subset <- filter(data_min_max,
+                 ID == "16031")
+
+subset$degr_360 <- NA
+subset <- subset[!is.na(subset$direction),]
+
+for (i in 1:dim(subset)[1]){
+  if (subset$direction[i] < 0){
+    subset$degr_360[i] = subset$direction[i] + 360
+  } else{
+    subset$degr_360[i] = subset$direction[i]
+  }}
+
+subset$bins <- cut(subset$degr_360, breaks = 72)
+
+ggplot(subset, aes(x = bins, y = depth_change)) +
+  stat_summary(fun = "mean", geom = "bar") + 
+  theme(axis.text.x = element_text(angle = 90, size = 14, vjust = 0.5, hjust=1))
 
 
 
