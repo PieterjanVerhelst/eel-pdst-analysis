b940eeaa1550e91c8a94c56c9dfca34c03f3f5fb
Pieterjan Verhelst <Pieterjan.Verhelst@UGent.be>
2020-08-06 13:06:53 +0200

    create plot for eel 15777

--- src/create_plots.R
+++ src/create_plots.R
@@ -25,13 +25,13 @@ data <- read_csv("./data/interim/data_circadian_tidal.csv",
 
 # Create subset per eel
 subset <- filter(data,
-                 ID == "16031")
+                 ID == "15777")
 
 
 # Create subset of several days for plot
 subset <- filter(data,
-                 ID == "16031",
-                 datetime >= "2019-02-07 00:00:00", datetime <= "2019-02-13 00:00:00")
+                 ID == "15777",
+                 datetime >= "2018-12-22 16:00:00", datetime <= "2018-12-29 00:00:00")
 
 # Create line every 24 hours
 gnu <-  seq.POSIXt(from = lubridate::floor_date(subset$datetime[1], "day"), to= subset$datetime[nrow(subset)], by = 86400)
