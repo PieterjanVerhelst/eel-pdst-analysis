c984473ede523471680ccdc3a457f2409a460afc
damianooldoni <damiano.oldoni@inbo.be>
2020-08-10 18:41:00 +0200

    Group by ID (eel) to find previous_sunset and next_sunset

--- src/link_circadian.R
+++ src/link_circadian.R
@@ -86,40 +86,78 @@ sun <- sun %>%
 # Merge sunrise and sunset data to data_circ dataset
 data_circ <- left_join(x = data_circ, y = sun, by=c("Date","avg_lat","avg_lon"))
 
-# Get ordered series of sunsets and sunrises
-series_sunsets <- unique(data_circ$sunset)
-series_sunsets <- series_sunsets[order(series_sunsets)]
+# Get ordered series of sunsets and sunrises per eel
+data_circ$sunset <- as.character(data_circ$sunset)
+data_circ$sunrise <- as.character(data_circ$sunrise)
 
-series_sunrises <- unique(data_circ$sunrise)
-series_sunrises <- series_sunrises[order(series_sunrises)]
+series_sunsets <- 
+  data_circ %>%
+  distinct(ID, sunset) %>%
+  arrange(ID, sunset) %>%
+  mutate(sunset = as_datetime(sunset))
 
-# Transform to df (tibble)
-series_sunsets <- tibble(sunset = series_sunsets)
-series_sunrises <- tibble(sunrise = series_sunrises)
 
-# Add previous sunset
+series_sunrises <- 
+  data_circ %>%
+  distinct(ID, sunrise) %>%
+  arrange(ID, sunrise) %>%
+  mutate(sunrise = as_datetime(sunrise))
+
+data_circ$sunset <- as_datetime(data_circ$sunset)
+data_circ$sunrise <- as_datetime(data_circ$sunrise)
+
+# Add previous sunset per each eel
 series_sunsets <-
   series_sunsets %>%
-  mutate(previous_sunset = lag(sunset))
+  group_by(ID) %>%
+  mutate(previous_sunset = lag(sunset)) %>%
+  ungroup()
 
-# Add next sunrise
+# Add next sunrise per each eel
 series_sunrises <-
   series_sunrises %>%
-  mutate(next_sunrise = lead(sunrise))
+  group_by(ID) %>%
+  mutate(next_sunrise = lead(sunrise)) %>%
+  ungroup()
+
+# Handle discontinuity in timeseries
+
+series_sunrises <- 
+  series_sunrises %>%
+  group_by(ID) %>%
+  mutate(next_sunrise_date = date(next_sunrise)) %>%
+  mutate(next_sunrise = if_else(
+    next_sunrise_date - lag(next_sunrise_date) <=  1,
+    next_sunrise,
+    NA_POSIXct_)) %>%
+  select(-next_sunrise_date) %>%
+  ungroup()
+
+series_sunsets <- 
+  series_sunsets %>%
+  group_by(ID) %>%
+  mutate(sunset_date = date(sunset)) %>%
+  mutate(previous_sunset = if_else(
+    sunset_date - lag(sunset_date) <=  1,
+    previous_sunset,
+    NA_POSIXct_)) %>%
+  select(-sunset_date) %>%
+  ungroup()
 
 # Identify day and night periods by using series_sunsets
 data_circ <-
   data_circ %>%
   left_join(series_sunsets,
-            by = c("sunset"))
+            by = c("ID", "sunset"))
 
 data_circ <-
   data_circ %>%
   left_join(series_sunrises,
-            by = c("sunrise"))
+            by = c("ID","sunrise"))
 
 data_circ <-
   data_circ %>%
+  group_by(ID) %>%
   mutate(start_sunmoment = case_when(
     datetime < sunrise ~ previous_sunset,
     datetime > sunset ~ sunset,
@@ -127,22 +165,23 @@ data_circ <-
     next_sunmoment = case_when(
       datetime > sunset ~ next_sunrise,
       datetime < sunrise ~ sunrise,
-      TRUE ~ sunset))
+      TRUE ~ sunset)) %>%
+  ungroup()
 
 # Add a progressive id to each night and day for each animal
 data_circ <-
   data_circ %>%
-  distinct(start_sunmoment) %>%
-  arrange(start_sunmoment) %>%
+  distinct(start_sunmoment, ID) %>%
+  arrange(start_sunmoment, ID) %>%
   mutate(n_day_phase = row_number()) %>%
   right_join(data_circ,
-             by = "start_sunmoment")
+             by = c("start_sunmoment", "ID"))
 
 # Add night_day column instead of 
 # data_circ$night_day <- ifelse(data_circ$datetime > data_circ$sunrise & data_circ$datetime < data_circ$sunset, 'day', 'night')
 data_circ <-
   data_circ %>%
-  mutate(nigth_day = if_else(datetime > sunset |
+  mutate(night_day = if_else(datetime > sunset |
                                datetime < sunrise,
                              "night",
                              "day"))
@@ -150,10 +189,10 @@ data_circ <-
 # Set sunset of day before (= start_sunmoment) and  sunrise of day after for all values of the night
 data_circ <-
   data_circ %>%
-  mutate(sunset = if_else(nigth_day == "night",
+  mutate(sunset = if_else(night_day == "night",
                           start_sunmoment,
                           sunset),
-         sunrise = if_else(nigth_day == "night",
+         sunrise = if_else(night_day == "night",
                            next_sunmoment,
                            sunrise))
 
