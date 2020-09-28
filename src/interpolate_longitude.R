8719e2fba3fd68e18c3941fd145e06b66166e0be
Pieterjan Verhelst <Pieterjan.Verhelst@UGent.be>
2020-09-28 14:52:02 +0200

    create long input file for eel A17521

--- src/interpolate_longitude.R
+++ src/interpolate_longitude.R
@@ -4,19 +4,19 @@
 
 # Create dataset with the following columns: Date/Time Stamp,	Long,	Quality
 # Date from eel release until tag retrieval
-input1 <- data.frame(seq(as.Date("2019/12/17"), as.Date("2020/02/15"), "days")) # take day before retrieval, since exact moment of retrieval is unknown
+input1 <- data.frame(seq(as.Date("2019/12/17"), as.Date("2020/06/04"), "days")) # take day before retrieval, since exact moment of retrieval is unknown
 colnames(input1)[1] <- "Date/Time Stamp"
 
 
 # Interpolate between longitude of release and retrieval location
-input1$Long <- seq(2.757437, -12.7, length.out = 61)
+input1$Long <- seq(2.757437, 4.72872, length.out = 171)
 
 # Add quality flag to longitude values
 # First and last line have highest certainty and therefore quality; these are flagged with '0'. Other get '5' (= worse quality)
 input1$Quality <- 5
 input1$Quality[1] <- 0
-input1$Quality[61] <- 0
+input1$Quality[171] <- 0
 
 
 # Write csv file
-write.csv(input1, "./data/interim/geolocation_input_files/input_A15789/EELA15789LONG.csv", row.names = FALSE)
+write.csv(input1, "./data/interim/geolocation_input_files/input_A17521/EELA17521LONG.csv", row.names = FALSE)
