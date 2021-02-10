cf98c7eece0508b80efd2f246023c5984c7f2142
Pieterjan Verhelst <Pieterjan.Verhelst@UGent.be>
2021-02-10 12:12:52 +0100

    link trajectory coordinates and circadian data to total track dataset

--- src/link_circadian.R
+++ src/link_circadian.R
@@ -33,7 +33,7 @@ tr_data <-  list.files(path = "./data/external/trajectory_data/",
   map_df(~read_csv(., col_types = cols(.default = "c"))) 
 
 # Select columns
-tr_data <- select(tr_data, ID, Date, MPL.Avg.Lat, MPL.Avg.Lon, Med.Lat, Med.Lon)
+tr_data <- dplyr::select(tr_data, ID, Date, MPL.Avg.Lat, MPL.Avg.Lon, Med.Lat, Med.Lon)
 tr_data <- rename(tr_data, 
                   avg_lat = MPL.Avg.Lat,
                   avg_lon = MPL.Avg.Lon,
@@ -135,7 +135,7 @@ data_circ$avg_lon <- as.numeric(data_circ$avg_lon)
 
 # 4. Link circadian phases to dataset ####
 # Calculate sunrise and sunset for each date + position
-m <- select(data_circ, Date, avg_lat, avg_lon)
+m <- dplyr::select(data_circ, Date, avg_lat, avg_lon)
 m <- m %>% 
   rename(
     date = Date,
@@ -200,7 +200,7 @@ series_sunrises <-
     next_sunrise_date - lag(next_sunrise_date) <=  1,
     next_sunrise,
     NA_POSIXct_)) %>%
-  select(-next_sunrise_date) %>%
+  dplyr::select(-next_sunrise_date) %>%
   ungroup()
 
 series_sunsets <- 
@@ -211,7 +211,7 @@ series_sunsets <-
     sunset_date - lag(sunset_date) <=  1,
     previous_sunset,
     NA_POSIXct_)) %>%
-  select(-sunset_date) %>%
+  dplyr::select(-sunset_date) %>%
   ungroup()
 
 # Identify day and night periods by using series_sunsets
@@ -268,8 +268,10 @@ data_circ <-
 
 
 # Rearrange columns
-data_circ <- select(data_circ, ID, datetime, numericdate, corrected_depth, temperature, Date, avg_lat, avg_lon, med_lat, med_lon, sunrise, sunset, previous_sunset, next_sunrise, start_sunmoment, next_sunmoment, night_day)
+data_circ <- dplyr::select(data_circ, ID, datetime, numericdate, corrected_depth, temperature, Date, avg_lat, avg_lon, med_lat, med_lon, sunrise, sunset, previous_sunset, next_sunrise, start_sunmoment, next_sunmoment, night_day)
 
 
 # 5. write csv ####
 write_csv(data_circ, "./data/interim/data_circadian_1hour.csv")
+#write.csv(all, "./data/interim/data_circadian_5min_totaltrack.csv")
+
