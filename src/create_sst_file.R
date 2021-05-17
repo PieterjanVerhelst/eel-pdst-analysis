64b683e018f6b27553713d4a71b161c6e887a89e
Pieterjan Verhelst <Pieterjan.Verhelst@UGent.be>
2021-05-17 14:00:12 +0200

    create new geolocation input files

--- src/create_sst_file.R
+++ src/create_sst_file.R
@@ -11,8 +11,8 @@ library(lubridate)
 Sys.setenv(TZ='GMT')
 
 # 1. Read in temperature and corrected pressure data ####
-temp_data <- read_csv("./data/interim/geolocation_input_files/input_A17668/EELA17668TEMP.csv")
-press_data <- read_csv("./data/interim/geolocation_input_files/input_A17668/EELA17668PRES.csv")
+temp_data <- read_csv("./data/interim/geolocation_input_files/input_A17663/EELA17663TEMP.csv")
+press_data <- read_csv("./data/interim/geolocation_input_files/input_A17663/EELA17663PRES.csv")
 
 
 # Merge them together
@@ -50,7 +50,7 @@ input_sst <- input_sst[,c(1,3,4,2)]
 
 
 # 4. Write csv files ####
-write.csv(input_sst, "./data/interim/geolocation_input_files/input_A17668/EELA17668TEMP_F.csv", na = "NaN", row.names = FALSE)
+write.csv(input_sst, "./data/interim/geolocation_input_files/input_A17663/EELA17663TEMP_F.csv", na = "NaN", row.names = FALSE)
 
 
 # Check 
--- src/interpolate_longitude.R
+++ src/interpolate_longitude.R
@@ -4,19 +4,19 @@
 
 # Create dataset with the following columns: Date/Time Stamp,	Long,	Quality
 # Date from eel release until tag retrieval
-input1 <- data.frame(seq(as.Date("2020/12/25"), as.Date("2021/02/19"), "days")) # take day before retrieval, since exact moment of retrieval is unknown
+input1 <- data.frame(seq(as.Date("2020/12/06"), as.Date("2021/04/10"), "days")) # take day before retrieval, since exact moment of retrieval is unknown
 colnames(input1)[1] <- "Date/Time Stamp"
 
 
 # Interpolate between longitude of release and retrieval location
-input1$Long <- seq(2.757437, -5.6421, length.out = 57)
+input1$Long <- seq(2.757437, -4.547764, length.out = 126)
 
 # Add quality flag to longitude values
 # First and last line have highest certainty and therefore quality; these are flagged with '0'. Other get '5' (= worse quality)
 input1$Quality <- 5
 input1$Quality[1] <- 0
-input1$Quality[57] <- 0
+input1$Quality[126] <- 0
 
 
 # Write csv file
-write.csv(input1, "./data/interim/geolocation_input_files/input_A17487/EELA17487LONG.csv", row.names = FALSE)
+write.csv(input1, "./data/interim/geolocation_input_files/input_A17663/EELA17663LONG.csv", row.names = FALSE)
