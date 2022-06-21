fabebb102f09f3f7209b9f3f62a9134060035c88
Pieterjan Verhelst <pieterjan.verhelst@inbo.be>
2022-06-21 15:25:12 +0200

    create actogram for depth amplitude

--- src/create_actogram.R
+++ src/create_actogram.R
@@ -102,7 +102,7 @@ data_1eel <-
   arrange(datetime)
 
 # Calculate rate of up and down vertical movement
-activity_threshold <- 2.5
+activity_threshold <- 1.5
 data_1eel$rate <- ((abs(data_1eel$corrected_depth - lag(data_1eel$corrected_depth, 1)))/60)*100
 data_1eel <- data_1eel %>%
   mutate(active = if_else(data_1eel$rate > activity_threshold,
@@ -149,12 +149,61 @@ a2
 
 
 
+# 3. Create actogram based on depth amplitude ####
 
+# Load data
+data <- read_csv("./data/interim/data_depth_diff.csv",
+                 na = "", 
+                 guess_max = 100000)
 
+data$...1 <- NULL
+data$ID <- factor(data$ID)
 
+# Select 1 eel
+data_1eel <- filter(data, ID == "16031")
 
+# Arrange data set according datetime
+data_1eel <-
+  data_1eel %>%
+  arrange(datetime)
 
+# Add month label option for plots
+data_1eel$month_abb <- month(data_1eel$datetime, label = TRUE, abbr = TRUE) 
 
+# Extract hours of the day (0 - 24)
+data_1eel$hour <- hour(data_1eel$datetime) 
+
+# Extract year day (0-365)
+data_1eel$yday <- yday(as.Date(data_1eel$datetime,"%Y-%m-%d")) 
+
+data_1eel$datehour <- lubridate::floor_date(data_1eel$datetime, "hour")  
+data_1eel$numericdatehour <- as.numeric(data_1eel$datehour)              
+data_1eel$day_number <- as.numeric(data_1eel$Date)
+
+# Create duplicate for double plot actogram
+data_1eel2 <- data_1eel
+data_1eel2 <- filter(data_1eel2, datehour > "2018-12-10 00:00:00")
+data_1eel2$hour <- 24+(data_1eel2$hour)
+data_1eel2$day_number <- data_1eel2$day_number -1
+
+data_1eel <- rbind(data_1eel, data_1eel2)
+
+# Just for visualisation purpose, add +1 hour
+data_1eel$hour <- 1+(data_1eel$hour)
+
+# Remove the single record at 2019-02-13 00:00:00 which results in a single cell on top of the plot
+data_1eel <- filter(data_1eel, day_number != "17940")
+
+# Create actogram
+a3 <- ggplot(data_1eel, aes(x=as.factor(hour), y=day_number, fill = depth_change))+ # where time is hours of the day (so, 0 to 24)
+  geom_tile()+
+  coord_equal() +
+  scale_fill_viridis(discrete=FALSE, name = 'Frequency of activity', option = 'viridis')+
+  ylab('day of year')+
+  xlab('hour of day')+
+  ylim(17870, 17940) +
+  theme_bw()
+a3
 
 
 
