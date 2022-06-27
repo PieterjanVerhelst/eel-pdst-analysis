69817a427e740978f5c0e3bbbefb905063ddfb60
Pieterjan Verhelst <pieterjan.verhelst@inbo.be>
2022-06-27 22:35:51 +0200

    create actogram with 15-min resolution

--- src/create_actogram.R
+++ src/create_actogram.R
@@ -369,24 +369,24 @@ data_1eel_summary$quarter_numeric <- as.numeric(data_1eel_summary$fquarter)
 data_1eel2 <- data_1eel_summary
 #data_1eel2 <- filter(data_1eel2, datehour > "2018-12-10 00:00:00")
 data_1eel2 <- filter(data_1eel2, day_number > 17874)
-data_1eel2$quarter <- 24+(data_1eel2$hour)
+data_1eel2$quarter_numeric <- 96+(data_1eel2$quarter_numeric)
 data_1eel2$day_number <- data_1eel2$day_number -1
 
 data_1eel_summary <- rbind(data_1eel_summary, data_1eel2)
 
 # Just for visualisation purpose, add +1 hour
-data_1eel_summary$hour <- 1+(data_1eel_summary$hour)
+#data_1eel_summary$hour <- 1+(data_1eel_summary$hour)
 
 # Remove the single record at 2019-02-13 00:00:00 which results in a single cell on top of the plot
 data_1eel_summary <- filter(data_1eel_summary, day_number != "17940")
 
 # Create actogram
-a5 <- ggplot(data_1eel_summary, aes(x=as.factor(hour), y=day_number, fill = average_depth))+ # where time is hours of the day (so, 0 to 24)
+a5 <- ggplot(data_1eel_summary, aes(x=as.factor(quarter_numeric), y=day_number, fill = average_depth))+ # where time is hours of the day (so, 0 to 24)
   geom_tile()+
   coord_equal() +
   scale_fill_viridis(discrete=FALSE, name = 'Frequency of activity', option = 'viridis')+
   ylab('day of year')+
-  xlab('hour of day')+
+  xlab('quarter of day')+
   ylim(17870, 17940) +
   theme_bw()
 a5
