4408b850e433c6b5cf9de3d5eb0a988ccb549e40
Pieterjan Verhelst <pieterjan.verhelst@inbo.be>
2022-07-27 10:19:01 +0200

    create boxplot

--- src/analyse_circadian_depth_diff.R
+++ src/analyse_circadian_depth_diff.R
@@ -22,7 +22,22 @@ data <- data[!is.na(data$night_day),]
 
 # Calculate summary
 aggregate(data$depth_change, list(data$night_day), mean)
+aggregate(data$depth_change, list(data$night_day), sd)
+aggregate(data$depth_change, list(data$night_day), min)
+aggregate(data$depth_change, list(data$night_day), max)
 aggregate(data$depth_change, list(data$night_day, data$ID), mean) # per eel
 
 # Create plot
-plot(data$night_day, data$depth_change)
+boxplot <- ggplot(data, aes(x=night_day, y=depth_change)) + 
+  geom_boxplot() +
+  theme_minimal() +
+  ylab("Depth difference (m)") +
+  xlab("Circadian phase") +
+  theme(axis.title.y = element_text(margin = margin(r = 10))) +
+  theme(axis.text.x = element_text(angle = 0, hjust = 1)) +
+  theme(axis.text = element_text(size = 20),
+        axis.title = element_text(size = 22)) #+
+  #ylim(0, 20)
+boxplot
+
+
